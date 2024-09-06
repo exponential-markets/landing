3b1fa933ee96bf4ce51ec260cf5cb674355d9154
Siddharth Movaliya <siddraj1992@gmail.com>
2024-09-06 16:53:54 +0530

    fix: add sticky navbar with responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => {
       <DeveloperSection />
       <Pricing />
       <Footer />
-      {/* <StickyNavbarTab /> */}
+      <StickyNavbarTab />
     </>
   );
 };
--- src/components/navbar/StickyNavbarTab.tsx
+++ src/components/navbar/StickyNavbarTab.tsx
@@ -5,13 +5,13 @@ const StickyNavbarTab = () => {
   const [activeLink, setActiveLink] = useState<string>("home");
 
   return (
-    <div className="hidden fixed top-6 rounded-full backdrop-blur-[8px] left-1/2 transform -translate-x-1/2 flex justify-center">
+    <div className="fixed bottom-1 md:bottom-6 lg:bottom-auto lg:top-10 xl:top-6 rounded-full backdrop-blur-[8px] left-1/2 transform -translate-x-1/2 flex justify-center">
       <div className="z-10 w-fit flex gap-2 px-2 border border-[#393939] rounded-full bg-[#0b0b0b] bg-opacity-20">
         {navLinks.map((link) => (
           <a
             key={link.id}
             href={`#${link.id}`}
-            className={`mx-4 my-3 ${
+            className={`m-2 lg:mx-4 lg:my-3 ${
               activeLink === link.id ? "text-white font-bold" : "text-gray-400"
             } hover:text-gray-300`}
             onClick={() => setActiveLink(link.id)}
