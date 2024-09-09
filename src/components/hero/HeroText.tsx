52906b13e57fb3d1995ad8a51f2bfb20e892feb9
Siddharth Movaliya <siddraj1992@gmail.com>
2024-09-09 19:50:22 +0530

    fix: line height for hero text

--- src/components/hero/HeroText.tsx
+++ src/components/hero/HeroText.tsx
@@ -1,7 +1,7 @@
 const HeroText = () => {
   return (
     <div className="mt-5">
-      <h1 className="w-full min-[820px]:w-fit text-center min-[820px]:text-left text-3xl lg:text-4xl xl:text-5xl font-medium lg:leading-[56px] text-transparent bg-gradient-to-r from-[#747478] via-white to-[#434345] bg-clip-text">
+      <h1 className="w-full min-[820px]:w-fit text-center min-[820px]:text-left text-3xl lg:text-4xl xl:text-5xl font-medium xl:leading-[56px] text-transparent bg-gradient-to-r from-[#747478] via-white to-[#434345] bg-clip-text">
         Unlock Algorithmic <br />
         Trading Like Never Before
       </h1>
